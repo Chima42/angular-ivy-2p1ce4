dd87484683a6ac5b680a00396f438a5a695dc608
Chima42 <me@chimannwosu.com>
2021-12-02 02:46:44 +0000

    3 files modified

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Title } from './title.model';
 import { TitleService } from './title.service';
 
@@ -12,20 +13,34 @@ import { TitleService } from './title.service';
 export class SimpleFormComponent implements OnInit {
   public titles$: Observable<Title[]>;
   public simpleForm: FormGroup;
+  public defaultSelectedOption: Title;
 
   constructor(private _titleService: TitleService) {}
 
   ngOnInit() {
+    this.titles$ = this._titleService
+      .getTitles()
+      .pipe(
+        tap(
+          (titles) =>
+            (this.defaultSelectedOption = titles.filter(
+              (title) => title.isDefault
+            )[0])
+        )
+      );
     this.simpleForm = new FormGroup({
-      title: new FormControl(''),
+      title: new FormControl('null'),
       firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      acceptTerms: new FormControl(''),
+      lastName: new FormControl('', [Validators.required]),
+      acceptTerms: new FormControl(false),
     });
-    this.titles$ = this._titleService.getTitles();
   }
 
   public onSubmit(form: FormGroup) {
-    console.log(form.value);
+    if (form.status === 'INVALID') {
+      return;
+    } else {
+      console.log(form);
+    }
   }
 }
